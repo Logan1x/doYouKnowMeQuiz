71c373de75635b73f389e7feb5e6aee54927d65c
Khushal Sharma <sharmakhushal78@gmail.com>
2021-08-19 06:09:55 +0000

    Renewed the layout

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
+var readlineSync = require('readline-sync');
+
+var score = 0;
+var readName = readlineSync.question('Your name please? ');
+console.log('\nWelcome '+readName+", let's see how much you know Me! \n");
+
+ function quiz (question, answer){
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+    console.log('You are right');
+    score++;
+    console.log("Score : ",score, "\n")
+
+  } else {
+    console.log('You are wrong '+'the right answer is '+ answer);
+    score--;
+    console.log("Score : ",score, "\n")
+
+  }
+  }
+
+  var question = [ {
+    question: "What is my fav programming language? ",
+    answer: "Python",
+  },{
+    question: "Which programming language did I learned first? ",
+    answer: "C",
+  },{
+    question: "What am I learning these days? ",
+    answer: "ReactJS",
+  },{
+    question: "What is my Age? ",
+    answer: "23",
+  }, {
+    question: "Which state I belong to? ",
+    answer: "Rajasthan",
+  },
+  ]
+
+  for (var i= 0 ; i < question.length; i++ ){
+  var currentQuestion = question[i];
+  quiz(currentQuestion.question,currentQuestion.answer)
+}
 
 
-var rs = require("readline-sync");
 
-console.log("Hello, Welcome to the Quiz!");
-
-var player = rs.question("Please enter your username : ");
-var welcomemsg = `Welcome ${player}`;
-
-console.log(welcomemsg);
-
-
-
-var answer = rs.question("Where do i study?");
-
-if (answer === "IIITD"){
-  console.log("True answer")
-}  
+console.log('Your final score is ' + score)
